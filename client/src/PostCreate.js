7b13fb188a18db1008fdc1a8d968f96e06bee6a1
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-07 15:15:54 +0545

    post services completed

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 export default () => {
-    return <div>
-        <form>
+    const [title, setTitle]= useState('');
+
+    const onSubmit = async(event) =>{
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts',{
+            title
+        });
+
+
+        
+        setTitle('');
+    };
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label>Title</label>
-                <input className="form-control" />
+                <input value={title} 
+                onChange={e => setTitle(e.target.value)} 
+                className="form-control" 
+                />
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
     </div>
-}
+    );
+};
