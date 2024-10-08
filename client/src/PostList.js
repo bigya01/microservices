efb76396b21ea8e04575cee3e03a656b80592394
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-08 18:30:10 +0545

    rendered comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
 
 
 export default () => {
@@ -24,7 +25,10 @@ export default () => {
         key = {post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
+
+                <CommentCreate  post={post.id}/>
             </div>
+
         </div>
         );
     });
