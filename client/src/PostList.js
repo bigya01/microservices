572402460a29871c651a0666d770c8ea3e8522fa
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-08 17:52:13 +0545

    adding functions for rendering posts

---
+++ client/src/PostList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+
+
+export default () => {
+    const [posts, setPosts] = useState ([]);
+
+
+    
+    return <div />;
+};
