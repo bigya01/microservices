de8b49c2ac22990a14f38f0626238167b09cc5bf
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-10-08 18:25:42 +0545

    fetching comments

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+export default ({postId}) => {
+    const [content, setContent]= useState('');
+
+
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <div className="form-gropu">
+                <label>New Comment</label>
+                <input 
+                value={content}
+                 onChange={
+                e => setContent(e.target.value)}
+                className="form-control"
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>;
+};
